ca6ec2e90a519f62e6cdeeba0ac9b51f060d0990
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2013-12-11 17:52:55 -0600

    add angular.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 8000,
+                port: 8080,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost'
             },
