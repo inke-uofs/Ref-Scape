848e7c9c87e94a70d6924a3df4109764899cba59
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2014-01-29 17:19:26 -0600

    tmp

--- js/controllers.js
+++ js/controllers.js
@@ -1,77 +1,19 @@
-var _ = require('underscore');
+var 
+_ = require('underscore'),
+$ = require('jQuery'), 
+util = require('util'),
+angular = require('angular'),
+URI = require('URIjs');
+
+exports.AppCtrl = ['$scope', function($scope){
+  var 
+  width = 960,
+  height = 500;
+
+  _.extend($scope, {
+    width: width,
+    height: height,
+  });
+}];
 
 
-exports.AppCtrl = function($scope){
-    var width = 960,
-    height = 500,
-    center = {x: width/2, y: height/2},
-    r = _.min([width, height])/4;
-
-    var data = {
-        items: [
-            {id: 1, user: 1, links: [5, 8]},
-            {id: 2, user: 2, links: [8, 3, 7, 9]},
-            {id: 3, user: 3},
-            {id: 4, user: 4, links: [8]},
-            {id: 5, user: 1},
-            {id: 6, user: 2},
-            {id: 7, user: 3, links: [3]},
-            {id: 8, user: 4, links: [4]},
-            {id: 9, user: 3}
-        ],
-        users: [
-            {id: 1, name: 'Xiaohan'},
-            {id: 2, name: 'Ben'},
-            {id: 3, name: 'Jade'},
-            {id: 4, name: 'Federica'},
-        ]
-    };
-
-    var 
-    index = 0,
-    users = {},
-    items = {},
-    nodes = [],
-    links = [],
-    tmpLinks = [],
-    angle = Math.PI * 2 / data.users.length;
-
-    _.each(data.users, function(user, i){
-        _.extend(user, {
-            x: center.x - Math.cos(angle * i) * r,
-            y: center.y + Math.sin(angle * i) * r,
-            fixed: true,
-            type: 'user',
-        });
-        nodes.push(user);
-        users[user.id] = index;
-        index += 1;
-    });
-    _.each(data.items, function(item){
-        _.extend(item, center, {type: 'item'});
-        nodes.push(item);
-        links.push({
-            source: index, target: users[item.user], type: 'user item'
-        });
-        if (item.links) {
-            tmpLinks.push({
-                source: index, target: item.links, type: 'item-item'
-            });
-        }
-        items[item.id] = index;
-        index += 1;
-    });
-    _.each(tmpLinks, function(link){
-        var type = link.type;
-        _.each(link.target, function(id){
-            links.push({source: link.source, target: items[id], type: type});
-        });
-    });
-
-    _.extend($scope, {
-        width: width,
-        height: height,
-        nodes: nodes,
-        links: links,
-    });
-};
--- js/directives.js
+++ js/directives.js
@@ -1,66 +1,234 @@
-var d3 = require('d3'),
-_ = require('underscore');
+var 
+_ = require('underscore'),
+$ = require('jQuery'), 
+util = require('util'),
+d3 = require('d3'),
+angular = require('angular'),
+URI = require('URIjs');
+
+var Request = function(data, callback) {
+  var self = this, 
+  request = document.createTextNode(JSON.stringify(data));
+
+  this.callback = callback;
+  this.request = request;
+
+  request.addEventListener("zotero-response",
+                           angular.bind(this, this.onResponse), false);
+  document.head.appendChild(request); 
+};
+
+Request.prototype.send = function() {
+  var evt = document.createEvent("HTMLEvents");
+  evt.initEvent("zotero-request", true, false);
+  this.request.dispatchEvent(evt);
+};
+
+Request.prototype.cleanRequest = function() {
+  var request = this.request;
+  this.request = null;
+  request.parentNode.removeChild(request);
+};
+
+Request.prototype.onResponse = function(evt) {
+  this.callback(JSON.parse(evt.target.nodeValue));
+  this.cleanRequest();
+};
+
+var Observer = function(data, callback) {
+  Request.call(this, {type: 'registerObserver', data: data}, callback);
+};
+
+util.inherits(Observer, Request);
+
+Observer.prototype.register = function() {
+  this.send();
+};
+
+Observer.prototype.unregister = function() {
+  var evt = document.createEvent("HTMLEvents");
+  evt.initEvent("unregist", true, false);
+  this.request.dispatchEvent(evt);
+  this.cleanRequest();
+};
+
+Observer.prototype.onResponse = function(evt) {
+  this.callback(JSON.parse(evt.target.nodeValue));
+};
+
+
 
 function getOrAppend(parent, selector) {
-    var target = parent.select(selector);
-    if (target.empty()) {
-        target = parent.append(selector);
-    }
-    return target;
+  var target = parent.select(selector);
+  if (target.empty()) {
+    target = parent.append(selector);
+  }
+  return target;
 }
 
-exports.d3 = function(){
-    // constants
-    var 
-    fill = d3.scale.category20(),
-    force = d3.layout.force().charge(-10).linkStrength(0.2).gravity(0);
+exports.d3 = ['$http', function($http){
+  // constants
+  var 
+  fill = d3.scale.category20(),
+  force = d3.layout.force().charge(-10).linkStrength(0.2).gravity(0);
 
-    return {
-        restrict: 'AE',
-        scope: {nodes: '=', links: '=', width: '=', height: '='},
-        link: function (scope, element, attrs) {
+  return {
+    restrict: 'AE',
+    scope: {width: '=', height: '='},
+    link: function (scope, element, attrs) {
+      var
+      angle,
+      width = scope.width,
+      height = scope.height,
+      center = {x: width/2, y: height/2},
+      r = _.min([width, height])/4,
+      index = 0,
+      users = {},
+      items = {},
+      nodes = force.nodes(),
+      links = force.links(),
+      size = {width: width, height: height},
+      svg = getOrAppend(d3.select(element[0]), 'svg').attr(size),
+      board = getOrAppend(svg, 'rect').attr(size),
+      node = svg.selectAll('.node'),
+      link = svg.selectAll('.link');
+
+      force.nodes(nodes).links(links).size(size).
+        on('tick', function(e){
+        link.attr({
+          x1: function(d) {return d.source.x;},
+          y1: function(d) {return d.source.y;},
+          x2: function(d) {return d.target.x;},
+          y2: function(d) {return d.target.y;},
+        });
+        node.attr({
+          cx: function(d) {return d.x;},
+          cy: function(d) {return d.y;}
+        });
+      });
+
+      node.on('click', function(){
+
+      });
+
+      function restart() {
+        link = link.data(links);
+        link.enter().insert('line', '.node').attr({
+          class: function(d){ return 'link ' + d.type; }
+        });
+
+        node = node.data(nodes);
+
+        node.enter().insert('circle', '.node').attr({
+          r: function(d) {
+            return (d.links || []).length * 2 + 5;
+          },
+          class: function(d){ return 'node ' + d.type;},
+        }).style('fill', function(d){return fill(d.user);}).call(force.drag);
+
+        force.start();
+      }
+
+
+      var groupID = 214824;
+      var server_uri = new URI('https://api.zotero.org/groups/' + groupID)
+      server_uri.addQuery({
+        key: 'qBvcuulbebWanSPiPurAuM9K',
+        content: 'none',
+      });
+
+      var api = {
+        get: function (segment, querys) {
+          var url = server_uri.clone().segment(segment).addQuery(querys);
+          return $http.get(url.normalize().toString());
+        },
+      };
+
+      api.get('items', {}).success(function(data, status, headers, config){
+        var keys = [];
+        _.each($(data).find('>entry'), function(entry){
+          var $entry = $(entry),
+          username = $entry.find('>author>name').text(),
+          key = $entry.find('>zapi\\:key').text();
+
+          if (!(username in users)) {
+            var user = {name: username, uri: $entry.find('>author>uri').text()};
+            users[username] = user;
+          }
+          items[key] = {user: username, key: key};
+          keys.push('\''+key+'\'');
+        });
+
+        angle = Math.PI * 2 / _.keys(users).length;
+        _.each(_.values(users), function(user, i){
+          _.extend(user, {
+            x: center.x - Math.cos(angle * i) * r,
+            y: center.y + Math.sin(angle * i) * r,
+            fixed: true,
+            type: 'user',
+          });
+          user.index = nodes.length;
+          nodes.push(user);
+        });
+
+        var request = new Request({
+          type: 'db', database: 'cobib', data: 
+            'select * from sync where groupID = \'' + groupID + '\''
+        }, function(response){
+          console.log('----------------');
+          console.log(response);
+        });
+        request.send();
+
+        var query = 'select * from items where key in (' + keys.join(', ') + ')';
+        var request = new Request({
+          type: 'db', database: 'zotero', data: query
+        }, function(response){
+          _.each(response, function(item){
+            var item = _.extend(items[item.key], item, center, {type: 'item'});
+            var user = users[item.user];
+            item.index = nodes.length;
+            nodes.push(item);
+            if (user) {
+              links.push({
+                source: item.index, target: user.index, type: 'user item'
+              });
+            }
+          });
+          restart();
+        });
+        request.send();
+
+        var r2 = new Request({
+          type: 'db',  database: 'zotero', data: 
+            'SELECT source.key as source, target.key as target ' +
+            'FROM itemSeeAlso link ' + 
+            'JOIN items source on link.itemID = source.itemID ' +
+            'JOIN items target on link.linkedItemID = target.itemID'
+        }, function(response){
+          _.each(response, function(link){
             var 
-            size = {width: scope.width, height: scope.height},
-            svg = getOrAppend(d3.select(element[0]), 'svg').attr(size),
-            board = getOrAppend(svg, 'rect').attr(size),
-            nodes = scope.nodes,
-            links = scope.links,
-            node = svg.selectAll('.node').data(nodes),
-            link = svg.selectAll('.link').data(links);
+            source = items[link.source],
+            target = items[link.target];
+            if (source && target) {
+              links.push({
+                source: source.index, target: target.index, type: 'item item'
+              });
+            }
+          });
+          restart();
+        });
+        r2.send();
 
-            force.nodes(nodes).links(links).size(size).
-                on('tick', function(e){
-                link.attr({
-                    x1: function(d) {return d.source.x;},
-                    y1: function(d) {return d.source.y;},
-                    x2: function(d) {return d.target.x;},
-                    y2: function(d) {return d.target.y;},
-                });
-                node.attr({
-                    cx: function(d) {return d.x;},
-                    cy: function(d) {return d.y;}
-                });
-            });
+      });
 
-
-            node.on('click', function(){
-
-            });
-
-            node.enter().insert('circle', '.node').attr({
-                r: function(d) {
-                    return (d.links || []).length * 2 + 5;
-                },
-                class: function(d){ return 'node ' + d.type;},
-            }).style('fill', function(d){return fill(d.user);});
-
-
-            link.enter().insert('line', '.node').attr({
-                class: function(d){ return 'link ' + d.type; }
-            });
-
-            force.start();
-        }
-    };
-};
+      /*
+         var observer = new Observer(['item'], function(){
+         log += JSON.stringify(arguments);
+         element.text(log);
+         });
+         observer.register();*/
+    }
+  };
+}];
 
